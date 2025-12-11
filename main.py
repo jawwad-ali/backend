889e8c68332a7abafa7c3b39e21082bae3c4b898
Ali jawwad <33836051+jawwad-ali@users.noreply.github.com>
2025-12-11 19:41:17 +0500

    added post method

--- main.py
+++ main.py
@@ -3,9 +3,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+products = [
+    {
+        id: 1,
+        name: "Laptop",
+        price: 999.99
+    },
+    {
+        id: 2,
+        name: "Smartphone",
+        price: 499.99
+    }
+]
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/items/")
+def create_item(item: dict):
+    return products.append(item)
 
 if __name__ == "__main__":
     main()
